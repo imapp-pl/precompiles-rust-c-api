56b8128eb11b0c4713912caaa644b8f9741c1c94
Pawel Gebal <pawel.gebal@imapp.pl>
2022-12-13 18:33:34 +0100

    fix: fix warnings in c code

--- examples/ripemd160.c
+++ examples/ripemd160.c
@@ -3,14 +3,14 @@
 #include <stdint.h>
 #include "../precompiles.h"
 
-const uint8_t LENGTH = 32;
+#define LENGTH 20
 
 int main(void) {
     const uint8_t input_data[3] = {'a', 'l', 'a'};
     size_t input_size = 3;
-    uint8_t* result = (uint8_t*) malloc(LENGTH * sizeof(uint8_t));
+    uint8_t result[LENGTH] = {'0'};
 
-    ripemd160(input_data, input_size, result);
+    ripemd160(input_data, input_size, &result);
 
     for (size_t i = 0; i < LENGTH; i ++) {
         printf("%X", result[i]);
