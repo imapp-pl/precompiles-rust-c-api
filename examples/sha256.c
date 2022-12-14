0b6bb5b2ce9cc962f6dc4fddd6cea803a6262cf3
Pawel Gebal <pawel.gebal@imapp.pl>
2022-12-14 16:51:57 +0100

    feat: add blake2f

--- examples/sha256.c
+++ examples/sha256.c
@@ -8,7 +8,7 @@
 int main(void) {
     const uint8_t input_data[3] = {'a', 'l', 'a'};
     size_t input_size = 3;
-    uint8_t result[LENGTH] = {'0'};
+    uint8_t result[LENGTH] = {0};
 
     sha256(input_data, input_size, &result);
 
